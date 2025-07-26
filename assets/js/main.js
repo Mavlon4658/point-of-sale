8dcd11bc2c3eb706a49a4dfc2b7eaaaffd7265b3
Mavlon4658 <mavlonbe4658>
2025-07-26 14:21:52 +0500

    ready

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+const bodyHidden = () => {
+    document.querySelector('body').style.overflow = 'hidden';
+}
+
+const bodyVisible = () => {
+    document.querySelector('body').style.overflow = 'visible';
+}
+
+const phoneInp = document.querySelectorAll('input[type="tel"]');
+
+if (phoneInp.length) {
+    phoneInp.forEach(el => {
+        IMask(el, {
+            mask: '+{7}(000) 000-00-00',
+        })
+    });
+};
+
+const homeSwp = new Swiper('.home-bottom__swp .swiper', {
+    slidesPerView: 1.3,
+    loop: true,
+    effect: "coverflow",
+    centeredSlides: true,
+    spaceBetween: 0,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 115,
+        depth: 90,
+        modifier: 5
+    },
+    navigation: {
+        nextEl: '.home-bottom__swp .swp-btn__next',
+        prevEl: '.home-bottom__swp .swp-btn__prev',
+    },
+    pagination: {
+        el: '.home-bottom__swp .swp-pagination',
+        clickable: true,
+    }
+})
+
+const designSwp = new Swiper('.design-swp .swiper', {
+    slidesPerView: 1.3,
+    loop: true,
+    effect: "coverflow",
+    centeredSlides: true,
+    spaceBetween: 0,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 115,
+        depth: 90,
+        modifier: 5
+    },
+    navigation: {
+        nextEl: '.design-swp .swp-btn__next',
+        prevEl: '.design-swp .swp-btn__prev',
+    },
+    pagination: {
+        el: '.design-swp .swp-pagination',
+        clickable: true,
+    }
+})
+
+const clientsSwp = new Swiper('.clients-swp .swiper', {
+    slidesPerView: 1,
+    spaceBetween: 37.25,
+    breakpoints: {
+        1280: {
+            slidesPerView: 4,
+        },
+        991: {
+            slidesPerView: 3
+        },
+        500: {
+            slidesPerView: 2,
+        }
+    },
+    navigation: {
+        nextEl: '.clients-swp .swp-btn__next',
+        prevEl: '.clients-swp .swp-btn__prev',
+    },
+})
